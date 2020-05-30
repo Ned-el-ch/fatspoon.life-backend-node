d59c1c76fc3ed3e83912b51407cf8c3514fb5c1a
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-30 16:50:07 +0100

    addRecipe export done

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,19 +1,70 @@
-exports.addRecipe = (request, response, next) => {
+const User = require("../models/user");
+const Recipe = require("../models/recipe");
+const RecipeIngredient = require("../models/recipeIngredient");
 
+exports.addRecipe = (request, response, next) => {
+	User.findById(request.userData.userId)
+		.then(user => {
+			// console.log(user.recipes, request.body)
+			let newRecipe = new Recipe({
+				user: request.userData.userId,
+				title: request.body.title,
+				uuid: request.body.uuid,
+				description: request.body.description,
+				prepTime: request.body.prepTime,
+				cookingTime: request.body.cookingTime,
+				imageLink: request.body.imageLink,
+				instructions: request.body.instructions,
+				servingCount: request.body.servingCount
+			})
+			let ingData = request.body.recipeIngredients.map(item => {
+				return {
+					recipe: newRecipe._id,
+					ingredient: item._id,
+					weight: item.weight
+				}
+			})
+			RecipeIngredient.create(ingData).then(ingredients => {
+				newRecipe.recipeIngredients = ingredients;
+				user.recipes.push(newRecipe)
+				user.save()
+				newRecipe.save()
+					.then(recipe => {
+						console.log(recipe)
+						response.status(201).json({
+							recipe
+						})
+					})
+					.catch(error => {
+						console.log(error)
+						response.status(500).json({
+							error
+						})
+					})
+			})
+		})
 }
 
 exports.searchRecipes = (request, response, next) => {
-	
+
 }
 
 exports.getRecipe = (request, response, next) => {
-	
+
 }
 
 exports.updateRecipe = (request, response, next) => {
-	
+
 }
 
 exports.deleteRecipe = (request, response, next) => {
-	
+
+}
+
+exports.starRecipe = (request, response, next) => {
+
+}
+
+exports.unstarRecipe = (request, response, next) => {
+
 }
