f5e06195a2d56b2aae215f11a8d8b9374dcc6383
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-30 12:54:45 +0100

    Create recipes.js

---
+++ controllers/recipes.js
@@ -0,0 +1,19 @@
+exports.addRecipe = (request, response, next) => {
+
+}
+
+exports.searchRecipes = (request, response, next) => {
+	
+}
+
+exports.getRecipe = (request, response, next) => {
+	
+}
+
+exports.updateRecipe = (request, response, next) => {
+	
+}
+
+exports.deleteRecipe = (request, response, next) => {
+	
+}
