4bae5864f8ac973559443d3dd7ea4f6a841e8855
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-30 12:48:09 +0100

    add users/profile route

--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const checkAuth = require("../middleware/check-auth")
 
 const UsersController = require("../controllers/users")
 
-router.get("/", UsersController.allUsers);
 router.post("/signup", UsersController.createUser);
+router.get("/profile", checkAuth, UsersController.userProfile);
 router.post("/login", UsersController.loginUser);
 
 module.exports = router;
-
-
-
-
-// const express = require('express');
-// const router = express.Router();
-// const checkAuth = require("../middleware/check-auth")
-// const extractFile = require("../middleware/extract-file")
-// const PostsController = require("../controllers/posts")
-
-
-// router.post("", checkAuth, extractFile, PostsController.createPost);
-
-// router.get("", PostsController.getPosts);
-
-// router.get("/:id", PostsController.getPostById)
-
-// router.put("/:id", checkAuth, extractFile, PostsController.updatePost)
-
-// router.delete("/:id", checkAuth, PostsController.deletePost)
-
-// module.exports = router;
-
-
