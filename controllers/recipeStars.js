c7957a6e546f4a1fae158c433f253c5e5093be78
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-31 11:29:16 +0100

    add RecipeStar CD controllers

--- controllers/recipeStars.js
+++ controllers/recipeStars.js
@@ -1,7 +1,61 @@
+const RecipeStar = require("../models/recipeStar")
+
 exports.starRecipe = (request, response, next) => {
-	
+	RecipeStar
+		.findOne({
+			user: request.userData.userId,
+			recipe: request.params.id
+		}).then(result => {
+			if (result) {
+				response.status(500).json({
+					error: "Already starred"
+				})
+			} else {
+				RecipeStar
+					.create({
+						user: request.userData.userId,
+						recipe: request.params.id
+					})
+					.then(recipeStar => {
+						response.status(201).json({
+							recipeStar
+						})
+					})
+					.catch(error => {
+						response.status(500).json({
+							error
+						})
+					})
+			}
+		})
+		.catch(error => {
+			response.status(500).json({
+				error,
+				message: "Something went wrong"
+			})
+		})
 }
 
 exports.unstarRecipe = (request, response, next) => {
-	
+	RecipeStar
+		.deleteOne({
+			user: request.userData.userId,
+			recipe: request.params.id
+		})
+		.then(result => {
+			if (result.deletedCount > 0) {
+				response.status(200).json({
+					success: "Found. Deleted successfully."
+				})
+			} else {
+				response.status(500).json({
+					error: "Not Found or Not Deleted."
+				})
+			}
+		})
+		.catch(error => {
+			response.status(500).json({
+				error
+			})
+		})
 }
